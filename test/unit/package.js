b5b686dc128c3f9295585c05678070dc1d09cb89
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-04-10 17:04:14 -0500

    chore(test): additional error cases
    provides examples of invalid commits and tests to validate it triggers
    the appropriate rules as expected
    
    Fixes: #1

--- test/unit/package.js
+++ test/unit/package.js
@@ -21,10 +21,89 @@ test('package', async (t) => {
   t.test('error cases', async (t) => {
     testCommit(t, {
       commit: fixtures.error['deps-breaking']
+    , description: 'Breaking changes with deps'
     , report: {
         valid: false
       , errors: [
           {
+            level: 2
+          , valid: false
+          , name: 'references-empty'
+          , message: 'references may not be empty'
+          }
+        ]
+      , warnings: []
+      }
+    })
+
+    testCommit(t, {
+      commit: fixtures.error['missing-body']
+    , description: 'body-empty'
+    , report: {
+        valid: false
+      , errors: [{
+          level: 2
+        , valid: false
+        , name: 'body-empty'
+        , message: 'body may not be empty'
+        }]
+      , warnings: []
+      }
+    })
+
+    testCommit(t, {
+      commit: fixtures.error['invalid-fixes-ref']
+    , description: 'footer-empty'
+    , report: {
+        valid: false
+      , errors: [
+          {
+            level: 2
+          , valid: false
+          , name: 'footer-empty'
+          , message: 'footer may not be empty'
+          }
+        , {
+            level: 2
+          , valid: false
+          , name: 'references-empty'
+          , message: 'references may not be empty'
+          }
+        ]
+      , warnings: []
+      }
+    })
+
+    testCommit(t, {
+      commit: fixtures.error['missing-issue-ref']
+    , description: 'footer with missing issue reference'
+    , report: {
+        valid: false
+      , errors: [
+          {
+            level: 2
+          , valid: false
+          , name: 'references-empty'
+          , message: 'references may not be empty'
+          }
+        ]
+      , warnings: []
+      }
+    })
+
+    testCommit(t, {
+      commit: fixtures.error['missing-footer']
+    , description: 'missing footer'
+    , report: {
+        valid: false
+      , errors: [
+          {
+            level: 2
+          , valid: false
+          , name: 'footer-empty'
+          , message: 'footer may not be empty'
+          }
+        , {
             level: 2
           , valid: false
           , name: 'references-empty'
