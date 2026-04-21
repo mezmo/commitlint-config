e6f13be5a62d24de6c20a4153e5eac0e3dc7aa1c
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-04-21 16:11:53 -0500

    chore(ci): add jenkins setup for ci
    Add a jenkins file to run linting and tests for the pull requests. we
    don't have publishing setup right now, so this will just make sure the
    project is passable until then
    
    Fixes: #1

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,8 @@ const logdna = require('eslint-config-logdna')
 module.exports = defineConfig([
   {
     'extends': [logdna]
+  , 'files': ['bin/**/*.js', 'lib/**/*.js', 'test/**/*.js', 'index.js', 'scripts/**/*.js']
+  , 'ignores': ['.tap/**', './.tap']
   , 'languageOptions': {
       ecmaVersion: 2022
     , sourceType: 'commonjs'
--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -3,4 +3,6 @@ npm run tap -- -R tap
 
 code=$?
 npm run tap replay -- -R junit --reporter-file .tap/test.xml
+npm run tap replay -- -R jsonstream --reporter-file .tap/tap.json
+
 exit $code
