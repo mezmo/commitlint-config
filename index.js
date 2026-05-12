f61325897571ee653b76766815347ce7bcd7d6b5
Eric Satterwhite <esatterwhite@wi.rr.com>
2026-05-12 12:25:30 -0500

    chore(rules): remove the subject casing rule
    This run is mostly not helpful and doesn't prevent anything that would
    other wise be helpful. It mostly prevents people from using acroyms in
    the subject, which is very valid
    
    Fixes: #17

--- index.js
+++ index.js
@@ -25,11 +25,6 @@ const config = {
   , 'header-max-length': [2, 'always', 72]
   , 'references-empty': [2, 'never']
   , 'scope-case': [2, 'always', 'lower-case']
-  , 'subject-case': [2, 'always', [
-      'sentence-case'
-    , 'start-case'
-    , 'lower-case'
-    ]]
   , 'subject-empty': [2, 'never']
   , 'subject-full-stop': [2, 'never', '.']
   , 'type-case': [2, 'always', 'lower-case']
